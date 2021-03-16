89f0441b9d50d85e7443346265e8a3c1335bb35d
akash shahriar <akashshahriar55@gmail.com>
2021-03-16 22:17:43 +0600

    animation done

--- app/src/main/java/com/cookietech/namibia/adme/ui/loginRegistration/LoginFragment.kt
+++ app/src/main/java/com/cookietech/namibia/adme/ui/loginRegistration/LoginFragment.kt
@@ -1,11 +1,14 @@
 package com.cookietech.namibia.adme.ui.loginRegistration
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.fragment.app.Fragment
+import androidx.transition.TransitionInflater
 import com.cookietech.namibia.adme.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class LoginFragment : Fragment() {
@@ -13,6 +16,7 @@ class LoginFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sharedElementEnterTransition = TransitionInflater.from(context).inflateTransition(android.R.transition.move)
         arguments?.let {
 
         }
@@ -24,6 +28,13 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val hyperspaceJumpAnimation = AnimationUtils.loadAnimation(context, R.anim.login_slide_animation)
+        login_container.startAnimation(hyperspaceJumpAnimation)
     }
 
     companion object {
