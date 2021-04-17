c83fe07dc0db4ec20d579ede881c45f2437041eb
DESKTOP-4UOPMEB\bisha <bishalimtiaz60@gmail.com>
2021-04-17 20:28:22 +0600

    service provider activity static design implemented

--- app/src/main/java/com/cookietech/namibia/adme/ui/client/home/MarkerClickDetailsDialog.kt
+++ app/src/main/java/com/cookietech/namibia/adme/ui/client/home/MarkerClickDetailsDialog.kt
@@ -1,10 +1,12 @@
 package com.cookietech.namibia.adme.ui.client.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.cookietech.namibia.adme.R
 import com.cookietech.namibia.adme.models.ServicesPOJO
@@ -35,6 +37,14 @@ class MarkerClickDetailsDialog(): BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpViews()
+        setUpClicks()
+    }
+
+    private fun setUpClicks() {
+        /**View Profile**/
+        btn_view_profile.setOnClickListener{
+           findNavController().navigate(R.id.marker_dialog_to_sp_activity)
+        }
     }
 
     private fun setUpViews() {
